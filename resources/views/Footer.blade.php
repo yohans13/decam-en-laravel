db647314f2c86539fb2f70207c34d0ee4541aad0
yohans13 <yohansperera3@gmail.com>
2021-02-27 09:31:57 +0530

    login session coded

--- resources/views/Footer.blade.php
+++ resources/views/Footer.blade.php
@@ -1,11 +1,11 @@
 <div class="card">
   <div class="card-header">
-    Quote
+    Decam
   </div>
   <div class="card-body">
     <blockquote class="blockquote mb-0">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+      <p>Decam Sri Lanka</p>
+      <footer class="blockquote-footer">Powered by :<cite title="Source Title"> yohan perera</cite></footer>
     </blockquote>
   </div>
 </div>
